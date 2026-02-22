2827bf6a12d6e66040ad1e66afd764279aabc386
dsak789 <dsak.official@gmail.com>
2026-02-23 03:36:38 +0530

    Frontend started working

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-7 bg-black">
+      <div className="text-4xl text-white font-bold font-mono tracking-[2rem]">
+        UAS
+      </div>
+
+      <div className="text-xl text-gray-400 font-bold tracking-[.4rem]">
+        Ultimate Authentication Services
+      </div>
+    </div>
+  );
+};
+
+export default App;
